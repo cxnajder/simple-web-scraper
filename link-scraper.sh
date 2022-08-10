f41a6b410e221c8f93c80a3f0a247c67ab34612e
cxnajder <cxnajder@github.com>
2022-08-10 11:02:42 +0200

    update

--- link-scraper.sh
+++ link-scraper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-wget -q -O - $1 | grep  -oP '(?<=<a href=").*?(?=")' | sort | uniq | grep -v "#"
+if [ "$1"=="" ]
+then
+	echo "*** YOU FORGOT THE URL ***"
+	echo "example: https://webscraper.io/test-sites/e-commerce/allinone"
+else
+	wget -q -O - $1 | grep  -oP '(?<=<a href=").*?(?=")' | sort | uniq | grep -v "#"
+fi
--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 ./subdir-lister.sh $1 > temp.txt
 
 #loop/read line
-#	script.sh $line
+#	script.sh $line &
 #done < file
 
 rm temp.txt
